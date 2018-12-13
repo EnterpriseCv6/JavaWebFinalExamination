e54b52f07cf23217664e7abf72d0cbc80b403dcf
EnterpriseCV6 <hyylester@163.com>
2018-12-13 20:32:57 +0800

    Update finalTest_ChatSys.sql
    增加了好友分组属性，修改了注释符号（原来的是错的）

--- finalTest_ChatSys.sql
+++ finalTest_ChatSys.sql
@@ -1,33 +1,33 @@
 CREATE DATABASE chatSys;
 USE chatSys;
-//用户表
+#用户表
 CREATE TABLE userTable(
 userid VARCHAR(15) PRIMARY KEY,
 upassword VARCHAR(20),
 userstatus INT
 );
-//用户信息表
+#用户信息表
 CREATE TABLE userInfo(
 userid VARCHAR(15) PRIMARY KEY,
 username VARCHAR(10),
-usersign VARCHAR(100),	//用户签名
-ctime DATE,	//账号创建时间
+usersign VARCHAR(100),	#用户签名
+ctime DATE,	#账号创建时间
 birth DATE,
 address VARCHAR(100),
 FOREIGN KEY userInfo(userid) REFERENCES userTable(userid) ON UPDATE CASCADE
    ON DELETE RESTRICT
 );
-//聊天信息
+#聊天信息
 CREATE TABLE chatInfo(
-sendid VARCHAR(15) PRIMARY KEY,	//发送人id
-receiveid VARCHAR(15),	//接收人id
-chattime DATE,	//聊天时间
-message VARCHAR(100),	//消息
+sendid VARCHAR(15) PRIMARY KEY,	#发送人id
+receiveid VARCHAR(15),	#接收人id
+chattime DATE,	#聊天时间
+message VARCHAR(100),	#消息
 FOREIGN KEY chatInfoSend(sendid) REFERENCES userTable(userid) ON UPDATE CASCADE
    ON DELETE RESTRICT,
    FOREIGN KEY chatInfoReceive(receiveid) REFERENCES userTable(userid)
 );
-//未处理的消息（当接收方未上线时暂存于此，上线后获取值并删除）
+#未处理的消息（当接收方未上线时暂存于此，上线后获取值并删除）
 CREATE TABLE unprocessedMessage(
 sendid VARCHAR(15) PRIMARY KEY,
 receiveid VARCHAR(15),
@@ -37,11 +37,13 @@ FOREIGN KEY unprocessedMessageSend(sendid) REFERENCES userTable(userid) ON UPDAT
    ON DELETE RESTRICT,
    FOREIGN KEY unprocessedMessageReceive(receiveid) REFERENCES userTable(userid)
 );
-//好友表
-CREATE TABLE frientInfo(
-userid VARCHAR(15) PRIMARY KEY,	//用户id
-friendid VARCHAR(15),	//好友id
-friendName VARCHAR(10) REFERENCES userInfo(username), //好友姓名
+#好友表
+DROP TABLE friendInfo 
+CREATE TABLE friendInfo(
+userid VARCHAR(15) PRIMARY KEY,	#用户id
+friendid VARCHAR(15),	#好友id
+friendName VARCHAR(10) REFERENCES userInfo(username), #好友名
+friendgroup VARCHAR(10), #好友所在分组
 FOREIGN KEY friendInfo(userid) REFERENCES userTable(userid) ON UPDATE CASCADE
    ON DELETE RESTRICT,
    FOREIGN KEY friendInfoFriend(friendid) REFERENCES userTable(userid)
