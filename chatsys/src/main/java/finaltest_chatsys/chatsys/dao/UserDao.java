70720762f4c53d2206ac2f36ca5d2dccfa707baa
liang98 <314436792@qq.com>
2018-12-23 12:48:30 +0800

    Update UserDao.java
    修改了UserDao
    完成了向数据库插入新注册用户的信息

--- chatsys/src/main/java/finaltest_chatsys/chatsys/dao/UserDao.java
+++ chatsys/src/main/java/finaltest_chatsys/chatsys/dao/UserDao.java
@@ -2,10 +2,21 @@ package finaltest_chatsys.chatsys.dao;
 
 
 import finaltest_chatsys.chatsys.entity.User;
+import org.apache.ibatis.annotations.Insert;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import java.sql.Date;
+import java.math.BigInteger;
+
 //用户Dao
-public interface UserDao {
+@Repository
+public interface UserDao extends JpaRepository<User, Long>
+{
     //添加用户
-    public void addUser(User user);
-    //通过id删除用户
-    public void deleteUserById(String id);
+    @Modifying
+    @Query(value = "insert into userTable values(?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8)", nativeQuery = true)
+    public void addUser(String userid, String upassword, String username, String usersign, Date ctime, Date birth, String address, int userstatus);
+
 }
