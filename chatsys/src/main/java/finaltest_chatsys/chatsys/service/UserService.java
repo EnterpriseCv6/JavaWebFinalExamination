763534785d74224019b5298ab18b326a8526f9d8
liang98 <314436792@qq.com>
2018-12-23 14:25:32 +0800

    Update UserService.java
    添加修改密码功能接口

--- chatsys/src/main/java/finaltest_chatsys/chatsys/service/UserService.java
+++ chatsys/src/main/java/finaltest_chatsys/chatsys/service/UserService.java
@@ -3,8 +3,12 @@ package finaltest_chatsys.chatsys.service;
 
 import java.sql.Date;
 
-// 实现用户添加接口
+// 实现用户添加、修改密码接口
 public interface UserService
 {
+    // 添加用户
     public void addUser(String userid, String upassword, String username, String usersign, Date ctime, Date birth, String address, int userstatus);
+
+    //修改密码
+    public void updatePassword(String newPassword, String userid);
 }
