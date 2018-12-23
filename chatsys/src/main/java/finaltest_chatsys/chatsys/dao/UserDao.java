bd45362bf3867206015f17033273f6d8f2eb3d3e
liang98 <314436792@qq.com>
2018-12-23 17:19:38 +0800

    Update UserDao.java
    增加返回个人信息操作
    增加修改个人信息操作
    增加登录后修改userstatus操作
    增加注销后修改userstatus操作

--- chatsys/src/main/java/finaltest_chatsys/chatsys/dao/UserDao.java
+++ chatsys/src/main/java/finaltest_chatsys/chatsys/dao/UserDao.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import java.sql.Date;
 import java.math.BigInteger;
+import java.util.List;
 
 //用户Dao
 @Repository
@@ -19,8 +20,27 @@ public interface UserDao extends JpaRepository<User, Long>
     @Query(value = "insert into userTable values(?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8)", nativeQuery = true)
     public void addUser(String userid, String upassword, String username, String usersign, Date ctime, Date birth, String address, int userstatus);
 
+    // 登入修改login
+    @Modifying
+    @Query(value = "update userTable set userstatus = 1 where userid = ?", nativeQuery = true)
+    public void loginStatus(String userid);
+
+    // 退出修改status
+    @Modifying
+    @Query(value = "update userTable set userstatus = 0 where userid = ?", nativeQuery = true)
+    public void logoffSattus(String userid);
+
     // 修改密码
     @Modifying
     @Query(value = "update userTable set upassword = ?1 where userid = ?2", nativeQuery = true)
     public void updatePassword(String newPassword, String userid);
+
+    // 返回个人信息
+    @Query(value = "select * from userTable where userid = ?", nativeQuery = true)
+    public User queryUserInformation(String userid);
+
+    // 修改个人信息
+    @Modifying
+    @Query(value = "update userTable set username = ?1, usersign = ?2, birth = ?3, address = ?4 where userid = ?5", nativeQuery = true)
+    public void updateInformation(String newName, String newSign, Date newBirth, String newAddress, String userid);
 }
