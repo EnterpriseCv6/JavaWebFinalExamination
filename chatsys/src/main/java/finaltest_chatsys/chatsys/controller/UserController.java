52072036dbdace9aaf22ea0e6ffa1b3657274211
liang98 <314436792@qq.com>
2018-12-23 17:25:11 +0800

    Update UserController.java
    修改注册用户控制，不再需要手动输入ctime和userstatus
    增加返回个人信息控制，修改个人信息控制，登入退出修改status控制

--- chatsys/src/main/java/finaltest_chatsys/chatsys/controller/UserController.java
+++ chatsys/src/main/java/finaltest_chatsys/chatsys/controller/UserController.java
@@ -5,10 +5,7 @@ import finaltest_chatsys.chatsys.entity.User;
 import finaltest_chatsys.chatsys.service.UserService;
 import finaltest_chatsys.chatsys.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.sql.Date;
@@ -24,13 +21,15 @@ public class UserController
     // 增加用户
     // 对应表单id
     @PostMapping(value = "add")
-    public String addUser(@RequestParam("") String userid, @RequestParam("") String upassword, @RequestParam("") String username,
-                          @RequestParam("") String usersign, @RequestParam("") Date ctime,
-                          @RequestParam("") Date birth, String address, @RequestParam("") int userstatus)
+    public String addUser(@RequestParam("") String userid, @RequestParam("") String upassword,
+                          @RequestParam("") String username, @RequestParam("") String usersign,
+                          @RequestParam("") Date birth, String address)
     {
         try
         {
-            service.addUser(userid, upassword, username, usersign, ctime, birth, address, userstatus);
+            long time = System.currentTimeMillis();
+            Date date = new Date(time);
+            service.addUser(userid, upassword, username, usersign, date, birth, address, 1);
             return "";
         }
         catch (Exception e)
@@ -39,10 +38,40 @@ public class UserController
         }
     }
 
+    // 登录
+    @PostMapping(value = "login")
+    public void login(String userid)
+    {
+
+        // 修改userstatus
+        service.login(userid);
+    }
+
+    // 退出
+    @PostMapping(value = "logoff")
+    public void logoff(String userid)
+    {
+        // 修改userstatus
+        service.logoff(userid);
+    }
+
     // 修改密码
-    @PostMapping(value = "updatepassword")
+    @PostMapping(value = "updatePassword")
     public void updatePassword(String newPassword, String userid)
     {
         service.updatePassword(newPassword, userid);
+    }
+
+    @RequestMapping(value = "queryUserInformation",method = RequestMethod.GET)
+    public User queryUserInformation(String userid)
+    {
+
+        return service.queryUserInformation(userid);
+    }
+
+    @PostMapping(value = "updateInformation")
+    public void updateInformation(String newName, String newSign, Date newBirth, String newAddress, String userid)
+    {
+        service.updateInformation(newName, newSign, newBirth, newAddress, userid);
     }
 }
