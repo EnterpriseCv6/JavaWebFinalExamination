79b4a5e073fcc57e6d1873dd0eb6fea33141facb
liang98 <314436792@qq.com>
2018-12-23 13:34:32 +0800

    Create UserController.java
    增加userController

---
+++ chatsys/src/main/java/finaltest_chatsys/chatsys/controller/UserController.java
@@ -0,0 +1,41 @@
+package finaltest_chatsys.chatsys.controller;
+
+
+import finaltest_chatsys.chatsys.entity.User;
+import finaltest_chatsys.chatsys.service.AddUserService;
+import finaltest_chatsys.chatsys.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.sql.Date;
+
+@RestController
+@RequestMapping(value = "user")
+public class UserController
+{
+    @Autowired
+    private AddUserService service;
+
+
+    // 增加用户
+    // 对应表单id
+    @PostMapping(value = "add")
+    public String addUser(@RequestParam("") String userid, @RequestParam("") String upassword, @RequestParam("") String username,
+                          @RequestParam("") String usersign, @RequestParam("") Date ctime,
+                          @RequestParam("") Date birth, String address, @RequestParam("") int userstatus)
+    {
+        try
+        {
+            service.addUser(userid, upassword, username, usersign, ctime, birth, address, userstatus);
+            return "";
+        }
+        catch (Exception e)
+        {
+            return "联系人已经存在!";
+        }
+    }
+}
